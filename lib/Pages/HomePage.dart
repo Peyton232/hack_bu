c83a8317570482c83d24f7ee15b0a75816185b32
Mary Plana <MaryCharmaineLoiPlana@my.unt.edu>
2021-02-07 06:17:48 -0600

    editted addexpense

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -137,42 +137,42 @@ Widget _getExpenses() {
   );
 }
 
-class Activity extends StatelessWidget {
-  final String name;
-  final double amount;
-  final Color color;
-  final String addSubtract;
-
-  Activity({
-    @required this.amount,
-    @required this.color,
-    @required this.name,
-    @required this.addSubtract,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Text(
-              '$addSubtract \$$amount',
-              style: TextStyle(
-                color: color,
-                fontSize: 20.0,
-              ),
-            ),
-          ),
-          Text(
-            name,
-            style: TextStyle(
-              fontSize: 20.0,
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
+// class Activity extends StatelessWidget {
+//   final String name;
+//   final double amount;
+//   final Color color;
+//   final String addSubtract;
+//
+//   Activity({
+//     @required this.amount,
+//     @required this.color,
+//     @required this.name,
+//     @required this.addSubtract,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+//       child: Row(
+//         children: <Widget>[
+//           Expanded(
+//             child: Text(
+//               '$addSubtract \$$amount',
+//               style: TextStyle(
+//                 color: color,
+//                 fontSize: 20.0,
+//               ),
+//             ),
+//           ),
+//           Text(
+//             name,
+//             style: TextStyle(
+//               fontSize: 20.0,
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
