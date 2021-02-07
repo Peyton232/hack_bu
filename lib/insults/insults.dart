33e9bf80f1bb540bbb94470adf7d3adadaf3e7bc
peyton <peyton.t131@gmail.com>
2021-02-06 18:34:09 -0600

    old master

---
+++ lib/insults/insults.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+import 'package:built_collection/built_collection.dart';
+import 'package:expense_manager/db/services/category_service.dart';
+import 'package:expense_manager/models/category_model.dart';
+import 'package:rxdart/rxdart.dart';
+
+class InsultBlock {
+  var badInsults = [];
+
+}
